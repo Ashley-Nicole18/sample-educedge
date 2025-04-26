1ad5eeb3e5da76f1e8962a68ed514e579f2576bc
Ashley-Nicole18 <ashleynicole.gellado-23@cpu.edu.ph>
2025-04-26 23:21:27 +0800

    sample component

---
+++ app/bookings/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AboutMe from '../components/about'
+
+const page = () => {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+        <AboutMe/>
+    </main>
+  )
+}
+
+export default page
